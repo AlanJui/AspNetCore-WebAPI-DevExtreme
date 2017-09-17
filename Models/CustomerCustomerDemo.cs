df6df46d9fa994a58550376150f2aa9758e26e96
Alan Jui <alanjui.1960@gmail.com>
2017-09-17 19:55:34 +0800

    Initialize project

---
+++ Models/CustomerCustomerDemo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication2.Models
+{
+    public partial class CustomerCustomerDemo
+    {
+        public string CustomerId { get; set; }
+        public string CustomerTypeId { get; set; }
+
+        public Customer Customer { get; set; }
+        public CustomerDemographics CustomerType { get; set; }
+    }
+}
